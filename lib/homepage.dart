1e142648c8fbc96bd6368d3b33e2c55bfa9b6e11
Sumina Khatiwada <suminakhatiwada987@gmail.com>
2021-12-08 14:36:24 +0545

    push

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_application/quizUI.dart';
+
 class Homepage extends StatefulWidget {
   @override
   _HomepageState createState() => _HomepageState();
 }
+
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-
     return Container(
       color: Colors.teal,
       child: ElevatedButton(
-
-          onPressed: (){
-
+          onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => Quiz()),);
+              MaterialPageRoute(builder: (context) => Quiz()),
+            );
           },
-          child: Text("Let's Start Quiz!!", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold) ,)),
+          child: Text(
+            "Let's Start Quiz!!",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          )),
     );
   }
 }
